5e68153c9f6506ee3902639ebd217eaddedd79b0
Luiz Carlos <luizcmarinho9@gmail.com>
2025-03-06 13:37:10 -0300

    Inserindo dados

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import org.example.persistence.EmployeeDAO;
+import org.example.persistence.entity.EmployeeEntity;
+import org.flywaydb.core.Flyway;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    private final static EmployeeDAO employeeDAO = new EmployeeDAO();
+
+    public static void main(String[] args) {
+        var flyway = Flyway.configure()
+                .dataSource("jdbc:mysql://localhost/jdbc-sample", "root", "root")
+                .load();
+        flyway.migrate();
+
+        var employee = new EmployeeEntity();
+        employee.setName("Luiz");
+        employee.setSalary(new BigDecimal("3500"));
+        employee.setBirthday(OffsetDateTime.now().minusYears(20));
+        System.out.println(employee);
+        employeeDAO.insert(employee);
+        System.out.println(employee);
+    }
+}
